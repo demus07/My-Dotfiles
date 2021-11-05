a6eb89664120b5d06806a5a2449a08195b2877d5
demus <84559645+demus07@users.noreply.github.com>
2021-11-05 09:09:11 +0530

    Add files via upload

--- qtile/config.py
+++ qtile/config.py
@@ -8,6 +8,7 @@ from libqtile.config import Click, Drag, Group, KeyChord, Key, Match, Screen
 from libqtile.command import lazy
 from libqtile import layout, bar, widget, hook
 from libqtile.lazy import lazy
+from libqtile.layout.floating import Floating
 from typing import List
 
 mod = "mod4"
@@ -128,21 +129,17 @@ keys = [
          ]
 
 group_names = [("", {'layout': 'monadtall'}),
-               ("", {'layout': 'monadtall'}),
-               ("", {'layout': 'monadtall'}),
-               ("", {'layout': 'monadtall'}),
-               ("", {'layout': 'monadtall'}),
-               ("", {'layout': 'monadtall'}),
-               ("", {'layout': 'monadtall'}),
+               ("", {'layout': 'monadtall'}),
+               ("", {'layout': 'monadtall'}),
+               ("", {'layout': 'monadtall'}),
+               ("", {'layout': 'monadtall'}),
                ]
 groups = [
     Group("", matches=[Match(wm_class=["Evince"])]),
-    Group("", matches=[Match(wm_class=["Brave-browser"])]),
-    Group("", matches=[Match(wm_class=["discord"])]),
-    Group("", matches=[Match(wm_class=["Spotify"])]),
-    Group("", matches=[Match(wm_class=["Alacritty"])]),
-    Group("", matches=[Match(wm_class=["firefox"])]),
-    Group("", matches=[Match(wm_class=["Virtualbox"])]),
+    Group("", matches=[Match(wm_class=["Brave-browser"])]),
+    Group("", matches=[Match(wm_class=["discord"])]),
+    Group("", matches=[Match(wm_class=["Spotify"])]),
+    Group("", matches=[Match(wm_class=["Alacritty"])]),
     ]
 
 
@@ -152,21 +149,22 @@ for i, (name, kwargs) in enumerate(group_names, 1):
 
 layout_theme = {"border_width": 0,
                 "border_normal": "#FFE6E6",
-                "margin":5 
+                "margin": 10
                 }
 layouts = [
     layout.MonadTall(**layout_theme),
     layout.Floating(**layout_theme)
 ]
 
-colors = [["#464660", "#464660"], # panel background            #0
-          ["#57CC99", "#57CC99"], # Underline boi               #1
-          ["#7197E7", "#7197E7"], # color for the clock         #2
-          ["#F3F1F5", "#F3F1F5"], # Group foreground            #3
-          ["#A77AC4", "#A77AC4"], # Color for Battery           #4
+colors = [["#282c34", "#282c34"], # panel background            #0
+          ["#F38181", "#F38181"], # internet module             #1
+          ["#f05c5c", "#f05c5c"], # internet symbol             #2
+          ["#08D9D6", "#08D9D6"], # Underline && calender       #3
+          ["#07c5c2", "#07c5c2"], # Calender symbol             #4
           ["#000000", "#000000"], # Foreground for the widgets  #5
-          ["#7197E7", "#7197E7"], # Layout widget color         #6
-          ["#393E46", "#393E46"], # Group Background color      #7
+          ["#AA96DA", "#AA96DA"], # RAM widget                  #6
+          ["#957bd1", "#957bd1"], # Symbol for ram              #7
+          ["#EEEBDD", "#EEEBDD"]  # Group foreground            #8
           ]
 
 
@@ -181,71 +179,93 @@ extension_defaults = widget_defaults.copy()
 
 def init_widgets_list():
     widgets_list = [
-            widget.GroupBox(font="Ubuntu Bold",
-                       fontsize = 12,
+            widget.Sep(
+                       linewidth = 0,
+                       padding = 6,
+                       foreground = colors[0],
+                       background = colors[0]
+                       ),
+              widget.Image(
+                       filename = "~/.config/qtile/icons/python.png",
+                       scale = "False",
+                       mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(myTerm)}
+    ),
+            widget.GroupBox(font="Font Awesome",
+                       fontsize = 13,
                        margin_y = 4,
                        margin_x = 0,
-                       padding_y = -5,
-                       padding_x = 5,
+                       padding_y = 0,
+                       padding_x = 4,
                        borderwidth = 3,
-                       active = colors[3],
-                       inactive = colors[3],
-                       rounded = True,
-                       highlight_color = colors[2],
+                       active = colors[8],
+                       inactive = colors[8],
+                       rounded = False,
+                       highlight_color = colors[0],
                        highlight_method = "line",
-                       this_current_screen_border = colors[2],
+                       this_current_screen_border = colors[3],
                        foreground = colors[0],
                        background = colors[0]
                        ),
             widget.Sep(
                        linewidth = 0,
-                       padding = 770,
-                       foreground = colors[2],
+                       padding = 500,
+                       foreground = colors[0],
                        background = colors[0]
                        ),
-               widget.TextBox(
-                       text = '',
-                       background = colors[0],
-                       foreground = colors[2],
-                       padding = -1,
-                       fontsize = 37
-                       ),
-              widget.CurrentLayoutIcon(
-                       custom_icon_paths = [os.path.expanduser("~/.config/qtile/icons")],
-                       foreground = colors[3],
-                       background = colors[2],
+             widget.TextBox(
+                       text = '  ',
+                       background = colors[7],
+                       foreground = colors[5],
                        padding = 0,
-                       scale = 0.7
+                       fontsize =16
                        ),
-              widget.CurrentLayout(
-                       foreground = colors[3],
-                       background = colors[2],
+            widget.Memory(
+                       foreground = colors[5],
+                       background = colors[6],
                        padding = 5
                        ),
-            widget.TextBox(
-                       text = '',
-                       background = colors[2],
-                       foreground = colors[4],
-                       padding = -1,
-                       padding_y = 2,
-                       fontsize = 37
+            widget.Sep(
+                       linewidth = 0,
+                       padding = 25,
+                       foreground = colors[0],
+                       background = colors[0]
                        ),
-            widget.Clock(
-                       foreground = colors[3],
-                       background = colors[4],
-                       format =" %a %d %b %R  "
+             widget.TextBox(
+                       text = '  ',
+                       background = colors[2],
+                       foreground = colors[5],
+                       padding = 0,
+                       fontsize = 16
+                       ),
+             widget.Net(
+                       interface = "wlp13s0",
+                       format = '{down} {up}',
+                       foreground = colors[5],
+                       background = colors[1],
+                       padding = 5
+                       ),
+             widget.Sep(
+                       linewidth = 0,
+                       padding = 25,
+                       foreground = colors[0],
+                       background = colors[0]
                        ),
               widget.TextBox(
-                       text = '',
+                       text = '  ',
                        background = colors[4],
-                       foreground = colors[0],
-                       padding = -1,
-                       fontsize = 37
+                       foreground = colors[5],
+                       padding = 0,
+                       fontsize = 16
+                       ),
+            widget.Clock(
+                       foreground = colors[5],
+                       background = colors[3],
+                       format =" %a %d %b-%R  "
                        ),
                 widget.Sep(
                        linewidth = 0,
-                       padding = 6,
-                       foreground = colors[4],
+                       padding = 25,
+                       foreground = colors[0],
                        background = colors[0]
                        ),
                   widget.Systray(
